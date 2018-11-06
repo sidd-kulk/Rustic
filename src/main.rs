e7828fbc70771d621f7815b0362bc59eba2cd205
Siddharth Kulkarni <sidcool1234@gmail.com>
2018-11-07 03:01:08 +0530

    Cleanup and initial tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+fn gcd(n: u32, m: u32) -> u32 {
+    if n == 0 {return m};
+
+    gcd(m%n, n)
+
+}
+
+#[test]
+fn test_gcd(){
+    assert_eq!(gcd(15,10), 5);
+    assert_eq!(gcd(3,10), 1)
 }
