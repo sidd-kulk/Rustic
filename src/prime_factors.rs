5dd47d12813a1d5bebe75a79456af9a7c65f957b
Siddharth Kulkarni <sidcool1234@gmail.com>
2019-05-03 14:40:54 +0530

    Adding tests for prime_factors

--- src/prime_factors.rs
+++ src/prime_factors.rs
@@ -15,3 +15,12 @@ fn prime_factors(i: i32) -> Vec<i32> {
 
     factors
 }
+
+
+#[test]
+fn test_prime_factors(){
+    assert_eq!(vec![2], prime_factors(2));
+    assert_eq!(vec![2,2], prime_factors(4));
+    assert_eq!(vec![2,3], prime_factors(6));
+    assert_eq!(vec![3,3,5,31], prime_factors(1395));
+}
