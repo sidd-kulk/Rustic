ebfa189038913e8eedb629079170f350aac59aa5
Siddharth Kulkarni <sidcool1234@gmail.com>
2019-06-25 16:22:09 +0530

    playing with structs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,20 @@
 fn main(){
-    println!("Hello World!!");
+    let person = Person { name: "My Name".to_string(),
+                          address: Address {street: 1,house_identifier: "".to_string(), pincode: "".to_string()} };
 
+    println!("{:?}", person)
+
+}
+
+#[derive(Debug)]
+struct Person{
+    name: String,
+    address: Address
+}
+
+#[derive(Debug)]
+struct Address{
+    street: u16,
+    house_identifier: String,
+    pincode: String
 }
